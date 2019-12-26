a964fb10e0acbc69e0905c6bf70055c4e75fb21b
yingtian666 <961832766@qq.com>
2019-12-26 17:19:38 +0800

    Update 数组去重.js

--- 数组去重.js
+++ 数组去重.js
@@ -1,5 +1,15 @@
 //ES6 中的 Set 去重
+//ES6 提供了新的数据结构 Set，Set 结构的一个特性就是成员值都是唯一的，没有重复的值
 
 function distinct(array){
   return Array.from(new Set(array))
 }
+
+//简化下
+function unique(array){
+  return [...Set(array)]
+}
+
+//再简化
+let unique = (arr) => [...new Set(arr)]
+
